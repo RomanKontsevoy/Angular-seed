918995ea94a8787ccfc5c0d981bea2dae17d5a07
RomanKontsevoy <1604881@gmail.com>
2017-07-07 12:27:33 +0300

    Tried to make glyficon-star

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  isFavorite = false;
+
+  onClick(){
+    this.isFavorite = !this.isFavorite;
+  }
+
+
+}
